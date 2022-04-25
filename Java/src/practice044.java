2b9b11f31e6ed6d5548936651938a54ddddbbd5a
寺井健 <teraiken@teraikennoMacBook-Air.local>
2022-04-25 12:36:51 +0900

    practice041~044.java

---
+++ Java/src/practice044.java
@@ -0,0 +1,13 @@
+
+public class practice044 {
+	public static void main(String[] args) {
+		int[] numbers = {3, 4, 9};
+		System.out.println("１桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int n : numbers) {
+			if (n == input) {
+				System.out.println("アタリ！");
+			}
+		}
+	}
+}
